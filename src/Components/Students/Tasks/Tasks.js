bf380123b692cbe5ed5689b7a6b420a64df801d9
Praveenive <praveenhb0610@gmail.com>
2023-07-03 20:30:43 +0530

    final

--- src/Components/Students/Tasks/Tasks.js
+++ src/Components/Students/Tasks/Tasks.js
@@ -14,7 +14,7 @@ export default function Tasks({tasks,setTasks}) {
         }
         let token =localStorage.getItem("token")
         const fetchAlltasks = async()=>{
-          const response = await fetch(`http://localhost:9090/task/viewtasks`,{
+          const response = await fetch(`https://capstone-backend-m4t7-praveenive.vercel.app/task/viewtasks`,{
             method:"GET",
             headers:{
                 "x-auth-token":token
